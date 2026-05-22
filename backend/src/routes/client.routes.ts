4adbc2238936c548dd05a83b359f705339611413
DaleneyRuben <daleney.fernando@gmail.com>
2026-05-22 18:57:42 -0400

    add get all and get by id client routes

--- backend/src/routes/client.routes.ts
+++ backend/src/routes/client.routes.ts
@@ -5,6 +5,8 @@ import { createClientSchema } from '../schemas/client.schema';
 
 const router = Router();
 
+router.get('/', clientController.getAll);
+router.get('/:id', clientController.getById);
 router.post('/', validate(createClientSchema), clientController.create);
 
 export default router;
