3c770b70a8c1e0f65b372d0e996a0df910fcc4ed
DaleneyRuben <daleney.fernando@gmail.com>
2026-05-23 17:49:42 -0400

    wire app routes into layout shell

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
+import { Layout } from './components/layout/Layout';
+import { DashboardPage } from './pages/DashboardPage';
+import { ClientsPage } from './pages/ClientsPage';
+import { PlansPage } from './pages/PlansPage';
+import { MenuImportPage } from './pages/MenuImportPage';
+import { ReportsPage } from './pages/ReportsPage';
+import { RenewalsPage } from './pages/RenewalsPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<div>Catering Manager</div>} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/clientes/*" element={<ClientsPage />} />
+        <Route path="/planes" element={<PlansPage />} />
+        <Route path="/menu" element={<MenuImportPage />} />
+        <Route path="/informes" element={<ReportsPage />} />
+        <Route path="/renovaciones" element={<RenewalsPage />} />
+      </Routes>
+    </Layout>
   );
 }
 
