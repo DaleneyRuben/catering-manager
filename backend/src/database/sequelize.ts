07d5067d768ecfc0ec95ae14042385e4360c9f7f
DaleneyRuben <daleney.fernando@gmail.com>
2026-05-23 18:57:46 -0400

    pass pg module explicitly via dialectModule to fix vercel bundling

--- backend/src/database/sequelize.ts
+++ backend/src/database/sequelize.ts
@@ -1,3 +1,4 @@
+import pg from 'pg';
 import { Sequelize } from 'sequelize-typescript';
 import Client from '../models/Client';
 import Plan from '../models/Plan';
@@ -5,6 +6,7 @@ import Subscription from '../models/Subscription';
 
 const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
   dialect: 'postgres',
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
