37a6eed586c360d4439b638d2a40c35dd5519c7c
DaleneyRuben <daleney.fernando@gmail.com>
2026-05-25 16:18:15 -0400

    eager load subscriptions and plan in client queries

--- backend/src/services/client.service.ts
+++ backend/src/services/client.service.ts
@@ -1,10 +1,14 @@
 import Client from '../models/Client';
+import Plan from '../models/Plan';
+import Subscription from '../models/Subscription';
 import { CreateClientDto } from '../schemas/client.schema';
+
+const INCLUDE_SUBSCRIPTION = [{ model: Subscription, include: [Plan] }];
 
 const create = (data: CreateClientDto) => Client.create(data as never);
 
-const findAll = () => Client.findAll();
+const findAll = () => Client.findAll({ include: INCLUDE_SUBSCRIPTION });
 
-const findById = (id: number) => Client.findByPk(id);
+const findById = (id: number) => Client.findByPk(id, { include: INCLUDE_SUBSCRIPTION });
 
 export default { create, findAll, findById };
