e2e324763a892b591ac60403f70a88b6eb1a3802
DaleneyRuben <daleney.fernando@gmail.com>
2026-06-13 00:48:42 -0400

    refactor backend devFlags to export checkIsWeekend wrapper

--- backend/src/utils/devFlags.ts
+++ backend/src/utils/devFlags.ts
@@ -1,1 +1,5 @@
-export const bypassWeekendCheck = process.env.BYPASS_WEEKEND === 'true';
+import { isWeekend } from 'date-fns';
+
+const bypassWeekend = process.env.BYPASS_WEEKEND === 'true';
+
+export const checkIsWeekend = (date: Date): boolean => (bypassWeekend ? false : isWeekend(date));
