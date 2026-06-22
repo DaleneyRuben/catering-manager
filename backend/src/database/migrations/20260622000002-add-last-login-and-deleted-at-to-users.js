cebdaa0839734c9b46ead37d462fe0c8524d870b
DaleneyRuben <daleney.fernando@gmail.com>
2026-06-22 11:17:58 -0400

    add migration for users.lastLoginAt and users.deletedAt

---
+++ backend/src/database/migrations/20260622000002-add-last-login-and-deleted-at-to-users.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('users', 'lastLoginAt', {
+      type: Sequelize.DATE,
+      allowNull: true,
+    });
+    await queryInterface.addColumn('users', 'deletedAt', {
+      type: Sequelize.DATE,
+      allowNull: true,
+      defaultValue: null,
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.removeColumn('users', 'lastLoginAt');
+    await queryInterface.removeColumn('users', 'deletedAt');
+  },
+};
