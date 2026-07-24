98b3e93141b2b28f3a3a490866b91b1d52331b3c
DaleneyRuben <daleney.fernando@gmail.com>
2026-07-24 15:09:32 -0400

    implement convert appointment into client and subscription

--- backend/src/services/evaluation/convert-appointment.ts
+++ backend/src/services/evaluation/convert-appointment.ts
@@ -1,10 +1,21 @@
+import Appointment from '../../models/Appointment';
 import { CreateClientDto } from '../../schemas/client.schema';
 import { CreateSubscriptionDto } from '../../schemas/subscription.schema';
+import { create as createClient } from '../client';
+import { create as createSubscription } from '../subscription';
 
 export const convertAppointment = async (
-  _appointmentId: number,
-  _clientData: CreateClientDto,
-  _subscriptionData: CreateSubscriptionDto,
+  appointmentId: number,
+  clientData: CreateClientDto,
+  subscriptionData: CreateSubscriptionDto,
 ) => {
-  throw new Error('not implemented');
+  const appointment = await Appointment.findByPk(appointmentId);
+  if (!appointment || appointment.subscriptionId) return null;
+
+  const client = await createClient(clientData);
+  const subscription = await createSubscription(client.id, subscriptionData);
+  if (!subscription) return null;
+  await appointment.update({ subscriptionId: subscription.id });
+
+  return { client, subscription };
 };
--- backend/src/services/evaluation/index.ts
+++ backend/src/services/evaluation/index.ts
@@ -3,3 +3,4 @@ export { updateAppointment } from './update-appointment';
 export { cancelAppointment } from './cancel-appointment';
 export { findPendingForAdmin } from './find-pending-for-admin';
 export { findForNutritionist } from './find-for-nutritionist';
+export { convertAppointment } from './convert-appointment';
