568a7c6dd02af9c799cd843667a4a7f3e73380a9
DaleneyRuben <daleney.fernando@gmail.com>
2026-07-26 00:17:31 -0400

    thread an optional transaction through client creation

--- backend/src/services/client/create.ts
+++ backend/src/services/client/create.ts
@@ -1,4 +1,6 @@
+import { Transaction } from 'sequelize';
 import Client from '../../models/Client';
 import { CreateClientDto } from '../../schemas/client.schema';
 
-export const create = (data: CreateClientDto) => Client.create(data as never);
+export const create = (data: CreateClientDto, transaction?: Transaction) =>
+  transaction ? Client.create(data as never, { transaction }) : Client.create(data as never);
