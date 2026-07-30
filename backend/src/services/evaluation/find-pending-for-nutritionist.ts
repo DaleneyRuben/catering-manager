525c73e516b05d5113109752cf62ef35c6ccf9c6
DaleneyRuben <daleney.fernando@gmail.com>
2026-07-30 16:46:06 -0400

    rename find-for-nutritionist to find-pending-for-nutritionist and narrow it to pending appointments

--- backend/src/services/evaluation/find-pending-for-nutritionist.ts
+++ backend/src/services/evaluation/find-pending-for-nutritionist.ts
@@ -1,15 +1,16 @@
 import { Op } from 'sequelize';
 import Appointment from '../../models/Appointment';
-import Subscription from '../../models/Subscription';
 import { appToday } from '../../utils/date';
 import { pruneStalePendingAppointments } from './_helpers';
 
-export const findForNutritionist = async () => {
+export const findPendingForNutritionist = async () => {
   await pruneStalePendingAppointments();
 
   return Appointment.findAll({
-    where: { date: { [Op.gte]: appToday() } },
-    include: [{ model: Subscription, attributes: ['paid'] }],
+    where: {
+      date: { [Op.gte]: appToday() },
+      subscriptionId: { [Op.is]: null },
+    },
     order: [
       ['date', 'ASC'],
       ['time', 'ASC'],
